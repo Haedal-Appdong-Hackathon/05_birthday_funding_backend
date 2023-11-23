d68d25ac2843b1f43a8860d7b90b974a46d97489
이재현 <fanta4715@naver.com>
2023-11-24 02:16:36 +0900

    feat: 기본 로그인 페이지 미사용
    기본 로그인 페이지 미사용 설정

--- birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/BirthdayFundingApplication.java
+++ birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/BirthdayFundingApplication.java
@@ -2,8 +2,9 @@ package com.just.birthdayFunding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class BirthdayFundingApplication {
 
 	public static void main(String[] args) {
