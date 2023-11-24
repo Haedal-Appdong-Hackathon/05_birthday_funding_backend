c6fa9b677916c71c1a4e684ee62deec060a61691
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-24 14:45:25 +0900

    [Feat]: ShopController 틀 추가

--- birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/controller/ShopController.java
+++ birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/controller/ShopController.java
@@ -1,13 +1,26 @@
 package com.just.birthdayFunding.controller;
 
+import com.just.birthdayFunding.core.anotation.TokenUserId;
+import com.just.birthdayFunding.dto.common.response.PagingResponse;
+import com.just.birthdayFunding.dto.shop.response.BuyGifticonResponse;
+import com.just.birthdayFunding.dto.shop.response.GifticonDto;
 import com.just.birthdayFunding.service.ShopService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/shop")
 public class ShopController {
     private final ShopService shopService;
+
+    @GetMapping
+    public PagingResponse<GifticonDto> getGifticonList(@RequestParam int page, @RequestParam String category) {
+        return null;
+    }
+
+    @PostMapping("/{gid}/buy")
+    public BuyGifticonResponse buyGifticon(@PathVariable Long gid, @TokenUserId Long userId) {
+        return null;
+    }
 }
