22391666e864ef351cb31caff4b8b0be2a7aefea
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-25 01:28:26 +0900

    [Fix]: 미구현 컨트롤러 주석

--- birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/controller/UserController.java
+++ birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/controller/UserController.java
@@ -1,28 +1,28 @@
-package com.just.birthdayFunding.controller;
-
-import com.just.birthdayFunding.core.anotation.TokenUserId;
-import com.just.birthdayFunding.dto.user.response.UserInfoResponse;
-import com.just.birthdayFunding.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/user")
-public class UserController {
-    private final UserService userService;
-
-    @GetMapping("/{userId}")
-    public UserInfoResponse getUserInfo(@TokenUserId Long userId) {
-        return userService.getUserById(userId).orElse(null);
-    }
-
-    @PutMapping()
-
-
-
-}
+//package com.just.birthdayFunding.controller;
+//
+//import com.just.birthdayFunding.core.anotation.TokenUserId;
+//import com.just.birthdayFunding.dto.user.response.UserInfoResponse;
+//import com.just.birthdayFunding.service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/user")
+//public class UserController {
+//    private final UserService userService;
+//
+//    @GetMapping("/{userId}")
+//    public UserInfoResponse getUserInfo(@TokenUserId Long userId) {
+//        return userService.getUserById(userId).orElse(null);
+//    }
+//
+//    @PutMapping()
+//
+//
+//
+//}
