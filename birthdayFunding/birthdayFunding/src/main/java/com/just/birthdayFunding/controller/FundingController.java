f780c8c39b98ceda074c09ff77d87e60179478f7
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-24 12:29:29 +0900

    [Feat]: FundingController 와이어프레임 생성

--- birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/controller/FundingController.java
+++ birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/controller/FundingController.java
@@ -1,13 +1,47 @@
 package com.just.birthdayFunding.controller;
 
+import com.just.birthdayFunding.core.anotation.TokenUserId;
+import com.just.birthdayFunding.dto.common.response.PagingResponse;
+import com.just.birthdayFunding.dto.funding.request.CloseFundingRequest;
+import com.just.birthdayFunding.dto.funding.request.CreateFundingRequest;
+import com.just.birthdayFunding.dto.funding.request.UpdateFundingRequest;
+import com.just.birthdayFunding.dto.funding.response.CloseFundingResponse;
+import com.just.birthdayFunding.dto.funding.response.FundingSummaryDto;
 import com.just.birthdayFunding.service.FundingService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/funding")
 public class FundingController {
     private final FundingService fundingService;
+
+    @GetMapping
+    public PagingResponse<FundingSummaryDto> getFundingList() {
+        return null;
+    }
+
+    @PostMapping
+    public Long createFunding(@RequestBody @Valid CreateFundingRequest dto, @TokenUserId Long userId) {
+        return null;
+    }
+
+    @PutMapping("/{fid}")
+    public void updateFunding(@PathVariable Long fid, @RequestBody UpdateFundingRequest dto, @TokenUserId Long userId) {
+
+    }
+
+    @PostMapping("/{fid}/join")
+    public void joinFunding(@PathVariable Long fid, @TokenUserId Long userId) {
+
+    }
+
+    @PostMapping("/{fid}/close")
+    public CloseFundingResponse closeFunding(@PathVariable Long fid, @RequestBody @Valid CloseFundingRequest dto, @TokenUserId Long userId) {
+        return null;
+    }
+
+
 }
