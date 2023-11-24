8b6e416a28ed8925d6b147be5df85a22d9ced82f
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-24 22:19:45 +0900

    [Feat]: dto 푸시

--- birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/dto/funding/request/JoinFundingRequest.java
+++ birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/dto/funding/request/JoinFundingRequest.java
@@ -3,10 +3,12 @@ package com.just.birthdayFunding.dto.funding.request;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class JoinFundingRequest {
     @NotNull(message = "기프리콘 리스트는 null이 될 수 없습니다.")
     @Size(min = 1, message = "기프리콘 리스트는 최소 1개 이상이어야 합니다.")
