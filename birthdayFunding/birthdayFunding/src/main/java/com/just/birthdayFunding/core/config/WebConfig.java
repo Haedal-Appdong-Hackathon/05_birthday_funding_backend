1bfb9e6e6341ac211f9185062bc9e2c00d1368f3
이재현 <fanta4715@naver.com>
2023-11-25 04:24:10 +0900

    feat: CORS 해결

--- birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/core/config/WebConfig.java
+++ birthdayFunding/birthdayFunding/src/main/java/com/just/birthdayFunding/core/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.just.birthdayFunding.core.config;
 import com.just.birthdayFunding.core.handler.TokenUserIdResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,5 +13,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new TokenUserIdResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(6000);
     }
 }
